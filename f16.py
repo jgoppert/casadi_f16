dcef447131491d678c1056c34467d384e5fdb746
James Goppert <james.goppert@gmail.com>
2019-08-21 16:17:50 -0400

    Move trim functions into f16.py

--- f16.py
+++ f16.py
@@ -651,3 +651,90 @@ def linearize(x0, u0, p0):
                       x=[f.name for f in x.fields()],
                       u=[f.name for f in u.fields()],
                       y=[f.name for f in x.fields()])
+
+def trim(s0, x: State, p: Parameters,
+         phi_dot: float, theta_dot: float, psi_dot: float, gam: float):
+
+    def constrain(x, s):
+        u = Control(thtl=s[0], elv_deg=s[1], ail_deg=s[2], rdr_deg=s[3])
+        alpha = s[4]
+        beta = s[5]
+
+        x.alpha = alpha
+        x.beta = beta
+
+        cos = ca.cos
+        sin = ca.sin
+        tan = ca.tan
+        atan = ca.arctan
+        sqrt = ca.sqrt
+
+        VT = x.VT
+        g = p.g
+        G = psi_dot*VT/g
+
+        a = 1 - G*tan(alpha)*sin(beta)
+        b = sin(gam)/cos(beta)
+        c = 1 + G**2*cos(beta)**2
+
+        # coordinated turn constraint pg. 188
+        phi = atan(G*cos(beta)/cos(alpha) *
+                   ((a - b**2) + b*tan(alpha)*sqrt(c*(1 - b**2) + G**2*sin(beta)**2))
+                   / (a**2 - b**2*(1 + c*tan(alpha)**2)))
+        x.phi = phi
+
+        # rate of climb constraint pg. 187
+        a = cos(alpha)*cos(beta)
+        b = sin(phi)*sin(beta) + cos(phi)*sin(alpha)*cos(beta)
+        theta = (a*b + sin(gam)*sqrt(a**2 - sin(gam)**2 + b**2)) \
+            / (a**2 - sin(gam)**2)
+        x.theta = theta
+
+        # kinematics pg. 20
+        x.P = phi_dot - sin(theta)*psi_dot
+        x.Q = cos(phi)*phi_dot + sin(phi)*cos(theta)*psi_dot
+        x.R = -sin(phi)*theta_dot + cos(phi)*cos(theta)*psi_dot
+
+        # engine power constraint
+        x.power = tables['tgear'](u.thtl)
+        return x, u
+
+    s = ca.MX.sym('s', 6)
+    x, u = constrain(x, s)
+    f = trim_cost(dynamics(x, u, p))
+    nlp = {'x': s, 'f': f}
+    S = ca.nlpsol('S', 'ipopt', nlp, {
+        'ipopt': {
+            'print_level': 0,
+        }
+    })
+    r = S(x0=s0, lbg=0, ubg=0)
+    s_opt = r['x']
+    x, u = constrain(x, s_opt)
+    return x, u
+
+
+def simulate(x0, u0, p, t0, tf, dt):
+    xs = ca.MX.sym('x', 13)
+    x = State.from_casadi(xs)
+    us = ca.MX.sym('u', 4)
+    u = Control.from_casadi(us)
+    dae = {'x': xs, 'p': us, 'ode': dynamics(x, u, p).to_casadi()}
+    F = ca.integrator('F', 'idas', dae, {'t0': 0, 'tf': dt, 'jit': True})
+    x = np.array(x0.to_casadi()).reshape(-1)
+    u = np.array(u0.to_casadi()).reshape(-1)
+    data = {
+        't': [0],
+        'x': [x]
+    }
+    t_vect = np.arange(t0, tf, dt)
+    for t in t_vect:
+        x = np.array(F(x0=x, p=u)['xf']).reshape(-1)
+        data['t'].append(t)
+        data['x'].append(x)
+    for k in data.keys():
+        data[k] = np.array(data[k])
+    return data
+
+
+
